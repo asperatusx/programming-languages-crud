68f8e20886a4b8d13a397536c139240f0c6ff78c
Andrew <andrewlang.p@gmail.com>
2024-12-14 01:05:43 -0500

    generate scaffold for programming languages to setup db with schema and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :programming_languages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "programming_languages#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+puts "Seeding started"
+
+ProgrammingLanguage.create([
+  { title: "Ruby", description: "A dynamic, open source programming language with a focus on simplicity and productivity.", link: "https://www.ruby-lang.org" },
+  { title: "Python", description: "A versatile programming language that emphasizes code readability.", link: "https://www.python.org" },
+  { title: "JavaScript", description: "A high-level programming language used for building web applications.", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  { title: "Go", description: "A statically typed, compiled language designed for scalability and performance.", link: "https://go.dev" },
+  { title: "Rust", description: "A language empowering everyone to build reliable and efficient software.", link: "https://www.rust-lang.org" }
+])
+
+puts "Seeding Done"
